75b70b98b42476843eacc655b852c61e166d2380
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-27 17:38:28 +0300

    Done

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {}
+
+  for (const number of array){
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+//
 /* 
   Add your pseudocode here
+
+  function hasTargetSum(array, target){
+    for (let i = 0; i < array.length; i++){
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++){
+        if (array[j] === complement)
+        return true;
+      }
+    }
+  }
 */
 
 /*
